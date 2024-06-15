474f64dfd666121bfc51b605acce60a046405006
Christian Peraza <97069248+chris-fisherman@users.noreply.github.com>
2024-06-15 20:13:14 -0300

    Revert "Finished Individual Activity"

--- src/js/product.js
+++ src/js/product.js
@@ -1,19 +1,15 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
-// Initialize the cart array from local storage if it exists, otherwise set it to an empty array.
-let cart = getLocalStorage("so-cart") || [];
-
-function addProductToCart(list) {
-  setLocalStorage("so-cart", list);
+function addProductToCart(product) {
+  setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
-  cart.push(product);
-  addProductToCart(cart);
+  addProductToCart(product);
 }
 
 // add listener to Add to Cart button
