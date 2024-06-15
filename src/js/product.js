3d8697b3fcbaa8fe55f962153f4f07adb9fc0dec
Braxton Hotton <bhotton25@gmail.com>
2024-06-14 22:07:48 -0600

    Finished Individual Activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,15 +1,19 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+// Initialize the cart array from local storage if it exists, otherwise set it to an empty array.
+let cart = getLocalStorage("so-cart") || [];
+
+function addProductToCart(list) {
+  setLocalStorage("so-cart", list);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  cart.push(product);
+  addProductToCart(cart);
 }
 
 // add listener to Add to Cart button
