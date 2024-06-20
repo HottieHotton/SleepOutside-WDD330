a646268611d75ddfcec9e2290e1f283c6fb2e71d
Braxton Hotton <bhotton25@gmail.com>
2024-06-19 20:44:06 -0600

    Fixed product.js weird conflict error

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,9 @@
 import { getParams } from "./utils.mjs";
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from './productdetails.mjs';
+
+const productId = getParams('product');
+const dataSource = new ProductData('tents');
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
