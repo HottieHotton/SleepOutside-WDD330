d6db226bc7d8c6fb1382388981f6ae5f09194972
Hans0l023 <watts.hans@gmail.com>
2024-06-22 13:30:36 -0500

    report, counter sorta works

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,5 @@
 import { getLocalStorage } from "./utils.mjs";
+import { updateCartCounter } from "./productdetails.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
@@ -10,19 +11,19 @@ function renderCartContents() {
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
+  <img
+  src="${item.Image}"
+  alt="${item.Name}"
+  />
   </a>
   <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
+  <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
+  </li>`;
+  
   return newItem;
 }
 
