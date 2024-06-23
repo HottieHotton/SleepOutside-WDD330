ff96d387bc376102402f869e8d5a2ad228f265bd
jeremyaaronha <jeremyaaronha@gmail.com>
2024-06-22 23:18:04 -0400

    save changes style.css and cart.js

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,20 +1,38 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   console.log(cartItems)
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  //add click event listener to each remove button
+  const removeButtons = document.querySelectorAll(".remove-item");
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const productId = event.target.dataset.id;
+      removeFromCart(productId);
+    });
+  });
+}
+
+function removeFromCart(productId) {
+  let cartItems = getLocalStorage("so-cart");
+  cartItems = cartItems.filter((item) => item.Id !== productId);
+  setLocalStorage("so-cart", cartItems);
+  renderCartContents();
 }
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
+  <button class="remove-item" data-id="${item.Id}">X</button>
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
       alt="${item.Name}"
     />
   </a>
+  
   <a href="#">
     <h2 class="card__name">${item.Name}</h2>
   </a>
