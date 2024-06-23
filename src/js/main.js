d3c8926d9e02ec8d6cbf3eb8915881de08b88a3e
Christian Peraza <christianperaza2000@gmail.com>
2024-06-22 23:41:01 -0300

    Alert.mjs Imported to main.js and an Instance was Created

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import Alert from "./Alert.mjs";
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const listing = new ProductList("Tents", dataSource, element);
+
+const alert = new Alert();
+alert.getAlerts();
 
 listing.init();
