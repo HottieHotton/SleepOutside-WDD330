22d174330b8ea578ddbd1ec6848026225cfc2377
Christian Peraza <christianperaza2000@gmail.com>
2024-06-22 23:34:54 -0300

    Constructor and getAlerts Function added to Alert.js

--- src/js/Alert.js
+++ src/js/Alert.js
@@ -1,3 +1,13 @@
 export default class Alert {
+    constructor() {
+        this.alertsPath = "../public/json/alerts.json";
+    }
 
+    getAlerts() {
+        return fetch(this.alertsPath)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    } 
 }
