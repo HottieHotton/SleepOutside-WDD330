dd4067570b6fa62468f603d9a6cf024a99cce668
Braxton Hotton <bhotton25@gmail.com>
2024-07-06 12:37:59 -0600

    Finished Search Bar

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -3,15 +3,29 @@ import ProductList from "./ProductList.mjs";
 import { updateCartCounter } from "./productdetails.mjs";
 import { loadHeaderFooter, getParams } from "./utils.mjs";
 
-const category = getParams("category");
-const dataSource = new ProductData();
-const element = document.querySelector(".product-list");
-const listing = new ProductList(category, dataSource, element);
 
-listing.init();
 
 async function init() {
   await loadHeaderFooter();
+  let form = document.getElementById("searchForm");
+  const category = getParams("category");
+  const dataSource = new ProductData();
+  const element = document.querySelector(".product-list");
+  let listing = new ProductList(category, dataSource, element);
+
+  listing.init();
+  
+  if (form) {
+    let searchButton = document.getElementById("searchButton");
+    searchButton.addEventListener("click", (event) => {
+      event.preventDefault(); // Prevent form from submitting normally
+      const searchQuery = document.getElementById("searchCall").value;      
+      listing = new ProductList(category, dataSource, element, searchQuery);
+
+      listing.init();
+    });
+  }
+
   await updateCartCounter();
 }
 
